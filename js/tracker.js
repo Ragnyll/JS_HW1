73e1b98f2493680e7778cb45460555cbfe0c3100
Ragnyll <justanotherjakeyg@gmail.com>
2016-11-02 14:59:53 -0500

    sets meowth location correctly

--- js/tracker.js
+++ js/tracker.js
@@ -14,24 +14,37 @@
   const transportHeading = document.getElementById('transportName');
 
   var map = L.map('map').setView([51.505, -0.09], 13);
+
+  L.tileLayer(osmUrl, {
+    attribution: osmAttrib
+  }).addTo(map); // is this needed?
+
+  // Marker initialization
+  var latLng = L.latLng(51.5, -0.09);
   var meowthMarker = L.icon({
     iconUrl: './images/meowth.png',
   });
-  L.marker([51.5, -0.09], {
+  var meowth = L.marker(latLng, {
     icon: meowthMarker
-  }).addTo(map)
-  L.tileLayer(osmUrl, {
-    attribution: osmAttrib
-  }).addTo(map);
+  }).addTo(map); // is it ok that it just starts here?
 
   (function updateProgress() {
     $.getJSON(trackerURL + '/position.json', function(data) {
+      // Progress bar stuff
       var progress = String((data['Progress'] * 100).toFixed(2)) + '%';
       var transport = String(data['Transport']);
       transportHeading.innerHTML = transport;
       progressBar.style.width = progress;
       progressBar.innerHTML = progress;
-      setTimeout(updateProgress, 500);
+
+      // Meowth Marker Stuff
+      var lat = data['Lat'];
+      var lng = data['Long'];
+      latLng.lat = lat;
+      latLng.lng = lng;
+      meowth.setLatLng(latLng);
+      console.log(meowth.getLatLng());
+      setTimeout(updateProgress, 3000);
     });
   }());
 
